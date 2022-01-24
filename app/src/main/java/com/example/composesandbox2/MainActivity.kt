f37691102998bd0b5567e3f45360bf052a6c2af4
Daniel <digity63@gmail.com>
2022-01-23 17:39:24 -0800

    Prospect RedCup App

--- app/src/main/java/com/example/composesandbox2/MainActivity.kt
+++ app/src/main/java/com/example/composesandbox2/MainActivity.kt
@@ -4,15 +4,13 @@ import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composesandbox2.ui.theme.ComposeSandbox2Theme
 
 class MainActivity : ComponentActivity() {
@@ -43,6 +41,11 @@ fun Main(){
         Surface(color = MaterialTheme.colors.primary, modifier = Modifier
             .fillMaxWidth()
             .weight(0.25f)){
+            Row(modifier = Modifier
+                .width(60.dp)
+                .height(3.dp) ){
+
+            }
 
         }
     }
