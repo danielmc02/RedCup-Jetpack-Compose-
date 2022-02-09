66f73184bcdac4d2125a1330a17c5f7610e70e50
Daniel <digity63@gmail.com>
2022-02-08 19:37:13 -0800

    Updated

--- app/src/main/java/com/example/composesandbox2/ui/theme/SignInButton.kt
+++ app/src/main/java/com/example/composesandbox2/ui/theme/SignInButton.kt
@@ -11,27 +11,32 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.composesandbox2.R
 
 
 @Composable
-fun GoogleSignInButton(){ val MyAppIcons = Icons.Filled
+fun GoogleSignInButton(){
 Button(onClick = { /*TODO*/ },
         modifier = Modifier
-            .background(MaterialTheme.colors.primary)
             .width(200.dp)
             .height(50.dp),
-        shape = CutCornerShape(20.dp)) {
-    Icon( MyAppIcons.Person, contentDescription = "Sign-In Icon")
+        shape = RoundedCornerShape(20.dp)) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_google__g__logo),
+        contentDescription = "Sign-In Icon",
+    tint = Color.Unspecified)
     Spacer(Modifier.size(ButtonDefaults.IconSpacing))
     Text(text = "Sign In", fontSize = 27.sp, color = MaterialTheme.colors.onPrimary)
 }
 
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun Screen(){
  GoogleSignInButton()
